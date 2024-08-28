bd25f7d744ab23f3409650a6e36cbbaa03796e5b
itshaneef <haneef8328@gmail.com>
2024-08-28 03:08:44 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*using System;
+
+namespace DistanceSpeed
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the speed (in meters per second):");
+            double speed = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the time (in seconds):");
+            double time = Convert.ToDouble(Console.ReadLine());
+
+            double distance = speed * time;
+
+            Console.WriteLine($"Distance traveled: {distance} meters");
+        }
+    }
+}
+
+
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace cuberoot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double cuberoot = Math.Cbrt(a);
+            Console.WriteLine($"Cube root of {a} is {cuberoot}");
+        }
+    }
+}*/
+
+
+using System;
+namespace cuberoot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
